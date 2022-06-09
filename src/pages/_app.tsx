2c0262adbb4599155ae264d6ac613669e04af501
Nicolas <nicolasst@drconsulta.com>
2022-06-09 01:59:37 -0300

    fix: hydratation problem on refresh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import "../globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+  const [showing, setShowing] = useState(false);
+
+  useEffect(() => {
+    setShowing(true);
+  }, []);
+
+  if (!showing) {
+    return null;
+  }
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    );
+  }
 }
 
 export default MyApp;
