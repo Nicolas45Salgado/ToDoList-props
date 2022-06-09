fe147afc7c32935e710e156c6105ca76fe6f30bc
Nicolas <nicolasst@drconsulta.com>
2022-06-09 00:36:27 -0300

    readme

--- src/atoms/text.ts
+++
@@ -1,7 +0,0 @@
-import { atom} from 'recoil';
-
-const textAtom = atom({
-  key: 'textAtom',
-  default: '',
-  dangerouslyAllowMutability: false, // In some cases it may be desireable to allow mutating of objects stored in atoms that don't represent state changes. Use this option to override freezing objects in development mode.
-})
